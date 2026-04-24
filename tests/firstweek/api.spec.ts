0930ac1f195cae17cc900a8ac7775dd1c4c7c3e3
Saravanan <saravananramesh020@gmail.com>
2026-04-24 20:00:20 +0530

    updated files

--- tests/firstweek/api.spec.ts
+++ tests/firstweek/api.spec.ts
@@ -27,4 +27,44 @@ expect(jsondata.userId).toBe(1)
 expect(jsondata.title).toBe("playwright API")
 expect(jsondata.body).toBe("API")
 })
+
+test('PUT api testing',async({request})=>{
+
+const putdata=await request.put('https://jsonplaceholder.typicode.com/posts/1',{
+
+data :{
+    id:1,
+    userId:100,
+    title:"Put api testing",
+    body:'API'
+}
+}
+)
+const jsondata= await putdata.json()
+console.log(putdata.status())
+expect(putdata.status()).toBe(200)
+expect(jsondata.userId).toBe(100)
+expect(jsondata.id).toBe(1)
+expect(jsondata.body).toBe('API')
+expect(jsondata.title).toBe('Put api testing')
+
+const patchupdate= await request.patch('https://jsonplaceholder.typicode.com/posts/1',{
+
+data :{
+    id:1,
+    title:'patch API'
+}}
+)
+const updatedata= await patchupdate.json()
+console.log(updatedata.id)
+console.log(updatedata.title)
+expect(updatedata.id).toBe(1)
+expect(jsondata.body).toBe('API')
+expect(updatedata.title).toBe('patch API')
+
+})
+
+
+
+
 })
