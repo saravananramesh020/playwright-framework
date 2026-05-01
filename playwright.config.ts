90e3f6bacd328c9cfb33160238204b69d898e710
Saravanan <saravananramesh020@gmail.com>
2026-05-01 19:32:52 +0530

    final commit - playwright framework

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,20 +16,29 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+ // forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+ // workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+  ['html'],
+  ['list'],
+  ['allure-playwright']
+],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+            headless: true,
+            screenshot: 'only-on-failure',
+            video:'retain-on-failure',
+            trace: 'on-first-retry',
+              
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    
   },
 
   /* Configure projects for major browsers */
--- tests/firstweek/day15.spec.ts
+++ tests/firstweek/day15.spec.ts
@@ -4,6 +4,6 @@ test('failure demo', async ({ page }) => {
   await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
 
   await page.waitForTimeout(3000);
-
+await expect(page.locator('h1')).toBe("text")
 
 });
